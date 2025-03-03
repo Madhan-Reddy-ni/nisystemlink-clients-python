288673fca386605b8639e4b0775db8b12576fd36
Priyadarshini Piramanayagam <priydarshini.piramanayagam@emerson.com>
2025-03-03 18:26:00 +0530

    add normlize method

--- nisystemlink/clients/testmonitor/utilities/_dataframe_utility.py
+++ nisystemlink/clients/testmonitor/utilities/_dataframe_utility.py
@@ -3,8 +3,61 @@ import pandas as pd
 
 from nisystemlink.clients.testmonitor._test_monitor_client import TestMonitorClient
 from nisystemlink.clients.testmonitor.models import (
+    QueryResultsRequest,
+    Result,
     ResultProjection
 )
+
+
+def __query_results_batched(
+    client: TestMonitorClient,
+    query_filter: str,
+    column_projection: Optional[List[ResultProjection]] = None,
+) -> List[Result]:
+    """Fetches results of a specific product in batches.
+
+    Args:
+        client: The TestMonitorClient to fetch results data.
+        query_filter: The filter to use when querying the results.
+        column_projection: List of columns to retrieve when querying the results.
+            Fields you do not specify are excluded. Returns all fields if no value is specified.
+
+    Returns:
+        List[Results]: A list of results.
+    """
+    all_results: List[Result] = []
+    results_query_request = QueryResultsRequest(
+        filter=query_filter,
+        projection=column_projection,
+        take=1000
+    )
+
+    results_query_response = client.query_results(results_query_request)
+    all_results.extend(results_query_response.results or [])
+
+    while results_query_response.continuation_token:
+        results_query_request.continuation_token = results_query_response.continuation_token
+        results_query_response = client.query_results(results_query_request)
+        all_results.extend(results_query_response.results or [])
+
+    return all_results
+
+
+def __normalize_results(results: List[Result]) -> pd.DataFrame:
+    """Normalizes the results into a Pandas DataFrame.
+
+    Args:
+        results: The list of results to normalize.
+
+    Returns:
+        A Pandas DataFrame with the normalized queried results.
+    """
+    results_dict = [
+        results.dict(exclude_unset=True) for results in results
+    ]
+    normalized_results = pd.json_normalize(results_dict, sep=".")
+
+    return normalized_results
 
 
 def get_results_dataframe(
@@ -17,10 +70,14 @@ def get_results_dataframe(
     Args:
         client: The TestMonitorClient to fetch results data.
         query_filter: The filter to use when querying the results.
-        column_projection: List of columns to retrieve from the queried results.
+        column_projection: List of columns to retrieve when querying the results. 
+            Fields you do not specify are excluded. Returns all fields if no value is specified.
 
     Returns:
         A Pandas DataFrame containing the results.
     """
-    results = client.get_results(query_filter=query_filter, column_projection=column_projection)
-    return pd.DataFrame(results)
+    queried_results = __query_results_batched(client, query_filter, column_projection)
+
+    results_dataframe = __normalize_results(queried_results)
+
+    return results_dataframe
